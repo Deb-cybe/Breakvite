b73c0904efbe2c26dd9e45e343e841b1aa7bd1a8
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 20:15:26 +0530

    Changed a bit.

--- src/components/greet.jsx
+++ src/components/greet.jsx
@@ -2,7 +2,7 @@ export default function Greet() {
   return (
     <>
       <div className="wrapper">
-        <h1>Welcome To My App</h1>
+        <h1>Welcome To My App!!!</h1>
         <p>This is going to be the coolest app in the world!</p>
       </div>
     </>
