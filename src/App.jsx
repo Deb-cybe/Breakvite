8c0d297dd8f86a17967ce0b0406efb79b6723173
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 20:45:24 +0530

    Connected with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import Greet from "./components/greet";
 export default function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("https://breaknode.onrender.com/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  });
   return (
     <>
       <div>Hello World</div>
+      <div>{message}</div>
       <Greet />
     </>
   );
